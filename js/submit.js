9f325a5fa5daa81c11cbb53914b4c26c1d946cc9
CyberCorps237 <cybercorps237@gmail.com>
2024-07-22 21:20:15 +0200

    contact form custom

--- js/submit.js
+++ js/submit.js
@@ -25,7 +25,7 @@ function validatePhone(phoneNumber) {
     return regex.test(phoneNumber);
 }
 
-
+// fonction qui verifie si ya l' erreur et affiche le message d'ereur
 let set_error = (element1,element2)=>{
     let error = document.querySelector(`.${element1}`);
     let error_border = document.querySelector(`.${element2}`);
@@ -33,6 +33,7 @@ let set_error = (element1,element2)=>{
     error.style.color='#b02a37';
     error_border.style.border='2px solid red';
 }
+// fonction de success des elements valide
 let set_success = (element1,element2)=>{
     let error = document.querySelector(`.${element1}`);
     let error_border = document.querySelector(`.${element2}`);
@@ -41,12 +42,18 @@ let set_success = (element1,element2)=>{
 }
 
 
-
+// fonction de soumission des formulaires
 const validateform = () =>{
     let user_name = nom.value.trim();
     let user_email = email.value.trim();
     let user_phone =phone.value.trim();
     let user_message =message.value.trim();
+
+    let success_name = true;
+    let success_email = true;
+    let success_phone = true;
+    let success_mess = true;
+
     let nom1 = 'nom_error';
     let nom2 = 'nom';
     let email1 = 'email_error';
@@ -58,30 +65,45 @@ const validateform = () =>{
 
     if(user_name === "" || user_name.length<4){
         set_error(nom1,nom2);
+        success_name = false;
     }else{
         set_success(nom1,nom2);
+        success_name = true;
     }
    
     if(user_message === "" || user_message.length<10){
         set_error(message1,message2);
+        success_mess = false;
     }else{
         set_success(message1,message2);
-        success_message.style.display="block";
-        
+        success_mess = true;
     }
 
     if(user_phone === "" || !validatePhone(user_phone)){
         set_error(phone1,phone2);
+        success_phone = false;
     }else{
         set_success(phone1,phone2);
-    
+        success_phone = true;
     }
     if(user_email === "" || !validateEmail(user_email)){
         set_error(email1,email2);
+        success_email = false;
     }else{
         set_success(email1,email2);
-    
+        success_email = true;
     }
-    
+
+    if(!success_name && !success_email && !success_phone && !success_mess ){
+        success_message.classList.add('alert-danger');
+        success_message.classList.remove('alert-secondary');
+        success_message.innerText = 'Error sending Message Please check the Items!!!';
+        success_message.style.display="block";
+    }else{
+        success_message.classList.add('alert-secondary');
+        success_message.classList.remove('alert-danger');
+        success_message.innerText = 'Message send Successfully! Thank you for your message!!!';
+        success_message.style.display="block";
+    }
 
 }
